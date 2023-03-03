150958b61c1369033af3c1710d322ce0e02477f8
ElMayo <mic.l.meier@gmail.com>
2023-03-03 18:30:02 +0100

    feat: initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: false,
+  proseWrap: 'always',
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
